40a17e58986d7fadb489271126a879ab467e02d4
Tuhin Roy <troy0870@gmail.com>
2018-01-01 11:12:19 +0530

    Corrected the indentation

--- driver.py
+++ driver.py
@@ -7,14 +7,13 @@ sample_file=open('sample.html','r')
 page = sample_file.read()
 
 #create an instance of BeautifulSoup to parse document
- soul = BeautifulSoup(page, 'html.parser')
+soul = BeautifulSoup(page, 'html.parser')
 
  #look for p tag
 
- reviews = soul.find_all('p')
+reviews = soul.find_all('p')
 
  #print each review
 
- for p in review:
- 	print p.get_text()
- 	
+for p in review:
+	print p.get_text()
